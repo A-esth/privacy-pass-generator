74af14def001821745066a681249d51b761c10ad
A-esth <92583956+A-esth@users.noreply.github.com>
2022-07-18 12:50:30 +0100

    Update index.ts

--- ts/src/index.ts
+++ ts/src/index.ts
@@ -9,7 +9,7 @@ const { hcaptchaToken } = require("puppeteer-hcaptcha");
   const startTime : number = Date.now();
 
   // Call hcaptchaToken method passing in your url
-  let token = await hcaptchaToken("https://csgostats.gg/match/72424083/watch/4466869596d606538282d5c9eceffd96c2bc91da60adbdfe480791d3ce022a10");
+  let token = await hcaptchaToken("https://captcha.website/");
 
   // Get End Time
   const endTime : number = Date.now();
